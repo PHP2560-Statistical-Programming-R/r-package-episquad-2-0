67a24de5cc7f8997ab7143aa94bcd110c3dd10b0
Sadia Sharmin <sadia_sharmin@brown.edu>
2017-12-01 18:43:03 -0500

    fixed error in my.table function

--- table function.R
+++ table function.R
@@ -1,31 +1,31 @@
 my.table <- function (data) {
   if (length(data)==4){
-    a<- aperm(array(data[1]:data[4], c(2,2,1)),perm=c(2,1,3)) #creates an array of 1 table 
+    a<- aperm(array(data[c(1:4)], c(2,2,1)),perm=c(2,1,3)) #creates an array of 1 table 
     dimnames(a) <- list(c("Yes","No"), c("Yes","No"))
     names(dimnames(a)) <- c("Disease", "Exposure")
-    tab.table<-list(factor.1=a[,,1]) #final list of tables
+    list.table<-list(factor.1=a[,,1]) #final list of tables
   } else if (length(data)==8){
-    a<-  aperm(array(data[1]:data[8], dim=c(2,2,2)), perm=c(2,1,3))#creates an array of 2 tables
+    a<-  aperm(array(data[c(1:8)], dim=c(2,2,2)), perm=c(2,1,3))#creates an array of 2 tables
     dimnames(a) <- list(c("Yes","No"), c("Yes","No"))
     names(dimnames(a)) <- c("Disease", "Exposure")
-    tab.table<-list(factor.1=a[,,1], factor.2=a[,,2])#final list of tables
+    list.table<-list(factor.1=a[,,1], factor.2=a[,,2])#final list of tables
   } else if (length(data)==12){
-    a<- aperm(array(data[1]:data[12], c(2,2,3)),perm=c(2,1,3))#creates an array of 3 tables
+    a<- aperm(array(data[c(1:12)], c(2,2,3)),perm=c(2,1,3))#creates an array of 3 tables
     dimnames(a) <- list(c("Yes","No"), c("Yes","No"))
     names(dimnames(a)) <- c("Disease", "Exposure")
-    tab.table<-list(factor.1=a[,,1], factor.2=a[,,2], factor.3=a[,,3])#final list of tables
+    list.table<-list(factor.1=a[,,1], factor.2=a[,,2], factor.3=a[,,3])#final list of tables
   }  else if (length(data)==16){
-    a<- aperm(array(data[1]:data[16], c(2,2,4)),perm=c(2,1,3))#creates an array of 4 tables
+    a<- aperm(array(data[c(1:16)], c(2,2,4)),perm=c(2,1,3))#creates an array of 4 tables
     dimnames(a) <- list(c("Yes","No"), c("Yes","No"))
     names(dimnames(a)) <- c("Disease", "Exposure")
-    tab.table<-list(factor.1=a[,,1], factor.2=a[,,2], factor.3=a[,,3], factor.4=a[,,4])#final list of tables
+    list.table<-list(factor.1=a[,,1], factor.2=a[,,2], factor.3=a[,,3], factor.4=a[,,4])#final list of tables
   }  else {
-    a<- aperm(array(data[1]:data[20], c(2,2,5)),perm=c(2,1,3))#creates an array of 5 table
+    a<- aperm(array(data[c(1:20)], c(2,2,5)),perm=c(2,1,3))#creates an array of 5 table
     dimnames(a) <- list(c("Yes","No"), c("Yes","No"))
     names(dimnames(a)) <- c("Disease", "Exposure")
-    tab.table<-list(factor.1=a[,,1], factor.2=a[,,2], factor.3=a[,,3], factor.4=a[,,4], factor.5=a[,,5])#final list of tables
+    list.table<-list(factor.1=a[,,1], factor.2=a[,,2], factor.3=a[,,3], factor.4=a[,,4], factor.5=a[,,5])#final list of tables
   }
-  return(tab.table)
+  return(list.table)
 }
 
 test.data<-c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
