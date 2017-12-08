b411d7b611d05c0c1b5f05158de45f8eb5d7d08f
Ze Zhang <ze_zhang@brown.edu>
2017-12-08 00:27:12 -0500

    Updates

--- epicalculator/R/risk_functions_SS.R
+++ epicalculator/R/risk_functions_SS.R
@@ -125,46 +125,56 @@ risk <- function (data, measure = c("RR", "RD"),
         r <- sum(wt.r)/sum(wt.st) # summary RR
         #calculations for ci:
         x <- log(r)
-        u[[i]] <- ((m1*n1*n0) - (a*c*t)) / (t^2)
+        u[[i]] <- ((m1*n1*n0) - (a*b*t)) / (t^2)
         l.A[[i]] <-  ((a*n0)/t)
-        l.B[[i]] <-  ((c*n1)/t)
+        l.B[[i]] <-  ((b*n1)/t)
         var.x <- sum(u) / (sum(l.A) * sum(l.B)) #final weighted variance
+        upper.ci<- exp(x + (qnorm(z) * sqrt(var.x)))
+        lower.ci<- exp(x - (qnorm(z) * sqrt(var.x)))
 
 
       } else { #calculate summary RD
-        wt.st[[i]] <- (n1^3 * n0^3 / ( (a * c) * n0^3 ) * (b * d1 * n1^3))
+        wt.st[[i]] <- (n1^3 * n0^3) / ((a*c*n0^3 ) + (b* d1*n1^3))
         r.st[[i]] <-  (a/n1) - (b/n0)
         wt.r <- wt.st * r.st #multiply the RR of each stratum with the weight of each stratum
         r <- sum(wt.r)/sum(wt.st) # summary RD
         x <- r
         var.x <- 1 / sum(wt.st)
+        upper.ci<- x + (qnorm(z) * sqrt(var.x))
+        lower.ci<- x - (qnorm(z) * sqrt(var.x))
       }
     }
-
-    #saving ci for output
-    upper.ci<- round(x + (qnorm(z) * sqrt(var.x)),4)
-    lower.ci<- round(x - (qnorm(z) * sqrt(var.x)), 4)
-
-    #summary hypothesis testing:
+#summary hypothesis testing (HP test):
     x.t <- c() #will contain the numerator of variance
     ex.x.t <- c() #will keep a part of denominator
     var.x.t <- c() #will keep another part of denominator
     df <- (1-dim(d[[1]])[1]) * (1-dim(d[[1]])[2])
 
-    for (i in 1:length(d)){ #this loop will calculate the stratum-specific estimates that we need for variance
+for (i in 1:length(d)){ #this loop will calculate the stratum-specific estimates for HP test
+      a <- d[[i]][1]
+      b <- d[[i]][3]
+      c <- d[[i]][2]
+      d1 <- d[[i]][4]
+      if (any(a <0 | b <0 | c <0 | d1 < 0))
+        stop("all entries of 'data' must be nonnegative and finite")
+      m1 <- a + b
+      m0 <- c + d1
+      n1 <- a + c
+      n0 <- b + d1
+      t <- a + b + c + d1
       x.t [[i]] <- a
       ex.x.t[[i]] <-  ((m1*n1)/t)
       var.x.t[[i]] <-  (m1*m0*n1*n0) / (t^3)
     }
 
     if (any(ex.x.t < 5))  warning("Chi-squared approximation may be incorrect")
-    z.sq<-round(((sum(x)- sum(ex.x.t))^2 / sum(var.x.t)), 2)
+    z.sq<-round(((sum(x.t)- sum(ex.x.t))^2 / sum(var.x.t)), 2)
   }
 
   #saving the results for final output
   risk<-rbind(Data=dt, round(r,4)) #risk contains data and RR/RD
   rownames(risk)[2] <- paste(estimate, measure, sep = " ") #paste RR or RD as the rowname
-  ci<-cbind(lower.ci, upper.ci) # contains upper and lower confidence interval
+  ci<-cbind(round(lower.ci,5), round(upper.ci,5)) # contains upper and lower confidence interval
   p <- round((pchisq(z.sq, df= 1, lower.tail = F)), 5)
   m <- "Pearson's Chi-squared test"
   test<-cbind(method = m,
@@ -175,6 +185,68 @@ risk <- function (data, measure = c("RR", "RD"),
   structure(list(risk,
                  Confidence.interval = ci,
                  Hypothesis.test = test)) # final output
+}
+
+
+
+
+
+#Function for hypothesis (HP) testing of crude and summary data:
+hp.test <- function(data, ci=95, estimate = c("crude", "summary")){
+  d <- my.table(data)
+  z<-1-((100-ci)/100)/2
+
+if (estimate == "crude") { #do HP test for crude data
+    if (length(d) > 1)
+      stop("can not have more than 4 values for 'crude data'")
+    a <- d[[1]][1]
+    b <- d[[1]][3]
+    c <- d[[1]][2]
+    d1 <- d[[1]][4]
+    if (any(a <0 | b <0 | c <0 | d1 < 0))
+      stop("all entries of 'data' must be nonnegative and finite")
+    m1 <- a + b
+    m0 <- c + d1
+    n1 <- a + c
+    n0 <- b + d1
+    t <- a + b + c + d1
+    df <- (1- dim(d[[1]])[1]) * (1-dim(d[[1]])[2])
+    x.t <- a
+    ex.t <- (m1*n1) / t
+    if (any(ex.t < 5))  warning("Chi-squared approximation may be incorrect")
+    var.x.t <- (m1*m0*n1*n0) / t^3
+    z.sq <- round(((x.t-ex.t)^2 / var.x.t), 2)
+
+
+} else {  #summary hypothesis testing:
+    x.t <- c() #will contain the numerator of variance
+    ex.x.t <- c() #will keep a part of denominator
+    var.x.t <- c() #will keep another part of denominator
+for (i in 1:length(d)){ #this loop will calculate the RR and weight of each stratum
+      a <- d[[i]][1]
+      b <- d[[i]][3]
+      c <- d[[i]][2]
+      d1 <- d[[i]][4]
+      m1 <- a + b
+      m0 <- c + d1
+      n1 <- a + c
+      n0 <- b + d1
+      t <- a + b + c + d1
+      df <- (1-dim(d[[1]])[1]) * (1-dim(d[[1]])[2])
+      x.t [[i]] <- a
+      ex.x.t[[i]] <-  ((m1*n1)/t)
+      var.x.t[[i]] <-  (m1*m0*n1*n0) / (t^3)
+    }
+    z.sq<-round(((sum(x.t)- sum(ex.x.t))^2 / sum(var.x.t)), 2)
+    z.sq
+  }
+  p <- round((pchisq(z.sq, df= 1, lower.tail = F)), 5)
+  m <- "Pearson's Chi-squared test"
+  test<-cbind(method = m,
+              z.square = z.sq,
+              df,
+              p.value = p) #contains method, test statistic, df and p
+  test
 }
 
 
