478f92c1e26f7b5e7c4a048675131038fe091ec4
cmueller <catrina.dpsi@gmail.com>
2017-12-09 14:34:46 -0500

    updates

--- epicalculator/R/OR.R
+++ epicalculator/R/OR.R
@@ -1,15 +1,14 @@
 #Function for calculating the Odds Ratio with Confidence Intervals
 # 95% Confidence Interval is the default setting
 OR<- function(table, ci=95){
-  x<- log((table[1,1]*table[2,2]/table[1,2]*table[2,1]))
+  x<- log((table[1,1]*table[2,2])/(table[1,2]*table[2,1]))
   OR<- ((table[1,1]*table[2,2])/(table[1,2]*table[2,1]))
   var<- ((1/table[1,1])+(1/table[1,2])+(1/table[2,1])+(1/table[2,2]))
   se<- sqrt(var)
   z<- 1-(.5*((100-ci)/100))
   upper.CI <- exp(x + (qnorm(z) * se))
   lower.CI <- exp(x - (qnorm(z) * se))
-  print(paste("Odds Ratio", round(OR, 2), ", ", ci, "% CI: ", "(", round(lower.CI,2), " to ", round(upper.CI,2), ")", sep=""))
-
+  print(paste("Odds Ratio is", signif(OR, 2), ",", ci, "% CI: ", "(", round(lower.CI,2), " to ", round(upper.CI,2), ")", sep=" "))
 }
 
 
