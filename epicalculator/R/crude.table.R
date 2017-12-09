393d1a595e6976399487226cd99442b47d72e641
Sadia Sharmin <sadia_sharmin@brown.edu>
2017-12-08 20:13:38 -0500

    crude.table function

---
+++ epicalculator/R/crude.table.R
@@ -0,0 +1,10 @@
+#input data is in a 2X2 table format where a represent exposed and diseased, b represent not exposed but diseased, c represent exposed person-time and d represent not exposed person-time
+crude.table<- function(a,b,c,d){
+  m1<-a+b #total diseased
+  t<-c+d #total person-time
+  tab<- matrix(c(a, b, m1, c, d, t), ncol=3, byrow = T) #create a matrix with the marginal values
+  colnames(tab) <- c('Exposure', 'No Exposure', "Total")
+  rownames(tab) <- c('Disease', 'PersonTime')
+  tab.table <- as.table(tab)
+  return(tab.table)
+}
